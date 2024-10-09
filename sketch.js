7c5ec836d00e45c67ad1311e460a58832263a676
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-09 11:39:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let myButton;
+let r;
+let g;
+let b;
+
 function setup() {
+  r=0;
+  g=0;
+  b=0;
   createCanvas(400, 400);
-}
+  myButton = createButton("click")
+  myButton.mousePressed(randomizeBackground)
+}//end setup
 
 function draw() {
-  background("#62B6CB");
-}
+  background(r,g,b);
+  myButton.position(0,200) 
+  
+}//end draw
+ function randomizeBackground(){
+   //this is going to randomly pick a number 
+   //between 0 and 255
+   r= random(255)
+   g= random(255)
+   b= random(255)
+ }//end randomizeBackground
