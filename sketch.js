e2730aef195876d2e3714104adecff5f53703d6a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-15 10:45:23 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,18 @@ let b;
 let myPicture;
 let myInput;
 let theirQuestion;
+let myAnswer = "I'll answer your question here."
+let possibleAnswer = [
+  "hard yes",
+  "no way",
+  "are you kidding me, there is no way!",
+  "sure"
+];
 
 function setup() {
-  r=0;
-  g=0;
-  b=0;
+  r=100;
+  g=100;
+  b=100;
   createCanvas(400, 400);
   myButton = createButton("click")
   myButton.mousePressed(randomizeBackground)
@@ -23,12 +30,19 @@ function draw() {
   myPicture.display();
   myButton.position(0,200) 
   text(theirQuestion, 0,250)
+  text(myAnswer,0,275)
 }//end draw
+function answerQuestion(){
+  let rndNumb = random(4);
+  rndNumb = Math.floor(rndNumb)
+  myAnswer = possibleAnswer[rndNumb]
+}//end answerQuestion
 function randomizeBackground(){
    //this is going to randomly pick a number 
    //between 0 and 255
    r= random(255)
    g= random(255)
    b= random(255)
-  theirQuestion = myInput.value();
+  theirQuestion = ">> " + myInput.value();
+  answerQuestion();
  }//end randomizeBackground
