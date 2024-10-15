b1847f0e93f0dc7d73bbbf604ac453a404a9a1cd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-15 10:14:14 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,32 +3,29 @@ let r;
 let g;
 let b;
 let myPicture;
+let myInput;
 
 function setup() {
   r=0;
   g=0;
   b=0;
-  
   createCanvas(400, 400);
   myButton = createButton("click")
   myButton.mousePressed(randomizeBackground)
-  myPicture = new Pictures(100,100,75,575,"peter.jpg")
+  myPicture = new Pictures(100,100,75,75,"peter.jpg")
+  myInput = createInput();
+  myInput.position(0,175)
 }//end setup
-
 function draw() {
   background(r,g,b);
   myPicture.display();
   myButton.position(0,200) 
   
 }//end draw
- function randomizeBackground(){
+function randomizeBackground(){
    //this is going to randomly pick a number 
    //between 0 and 255
    r= random(255)
    g= random(255)
    b= random(255)
  }//end randomizeBackground
-
-function Corbin(){
-  
-}
